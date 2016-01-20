c6d57ecce9672ce1bfdaddfce1b4653ac1cee398
hz <dxp@ngs.ru>
2016-01-20 18:28:23 +0600

    add sdram object

--- prj/src/main.cpp
+++ prj/src/main.cpp
@@ -1,48 +1,11 @@
 //******************************************************************************
 //*
-//*     FULLNAME:  Single-Chip Microcontroller Real-Time Operating System
+//*     ADSP-BF533/bfin-elf standalone program sample 
+//
+//*     Copyright (c) 2015-2016, Harry E. Zhurov
 //*
-//*     NICKNAME:  scmRTOS
-//*
-//*     PROCESSOR: ADSP-BF533 (Analog Devices)
-//*
-//*     TOOLKIT:   VDSP (Analog Devices)
-//*
-//*     PURPOSE:   Port Test File
-//*
-//*     Version: v5.0.1
-//*
-//*
-//*     Copyright (c) 2003-2015, scmRTOS Team
-//*
-//*     Permission is hereby granted, free of charge, to any person 
-//*     obtaining  a copy of this software and associated documentation 
-//*     files (the "Software"), to deal in the Software without restriction, 
-//*     including without limitation the rights to use, copy, modify, merge, 
-//*     publish, distribute, sublicense, and/or sell copies of the Software, 
-//*     and to permit persons to whom the Software is furnished to do so, 
-//*     subject to the following conditions:
-//*
-//*     The above copyright notice and this permission notice shall be included 
-//*     in all copies or substantial portions of the Software.
-//*
-//*     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-//*     EXPRESS  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-//*     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-//*     IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
-//*     CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-//*     TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
-//*     THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//*
-//*     =================================================================
-//*     Project sources: https://github.com/scmrtos/scmrtos
-//*     Documentation:   https://github.com/scmrtos/scmrtos/wiki/Documentation
-//*     Wiki:            https://github.com/scmrtos/scmrtos/wiki
-//*     Sample projects: https://github.com/scmrtos/scmrtos-sample-projects
-//*     =================================================================
 //*
 //******************************************************************************
-//*     ADSP-BF533/VisualDSP++ sample by Harry E. Zhurov, Copyright (c) 2005-2015
 
 #include <stdint.h>
 #include "device_def.h"
@@ -76,7 +39,7 @@ private:
 int f(int);
 int g(int);
 
-
+volatile uint16_t Array[] __attribute__ ((section(".sdram"))) = { 1, 2, 3, 4, 5, 6, 7, 8 };
 
 
 //---------------------------------------------------------------------------
