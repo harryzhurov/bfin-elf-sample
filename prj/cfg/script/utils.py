d9ecc2b014165a7b200659cc277340ec1971e5f7
hz <dxp@ngs.ru>
2016-01-21 20:02:57 +0600

    add print output secions sizes

--- prj/cfg/script/utils.py
+++ prj/cfg/script/utils.py
@@ -10,6 +10,7 @@
 
 import os
 import subprocess
+import re
 
 #-------------------------------------------------------------------------------
 # 
@@ -31,5 +32,47 @@ def pexec(cmd):
 
     return p.returncode, out, err
     
+#-------------------------------------------------------------------------------
+def print_size(datain):
+    
+    #--------------------------------------------
+    #
+    #    Get data
+    #
+    res  = re.search('\.text\s+(\d+)\s+(\d+)\n',  datain)
+    if res: csz = res.group(1)
+    else:   csz = 'none'
+     
+    res  = re.search('\.bss\s+(\d+)\s+(\d+)\n',  datain)
+    if res: bsz= res.group(1)
+    else:   bsz = 'none'
+               
+    res  = re.search('\.data\s+(\d+)\s+(\d+)\n',  datain)
+    if res: dsz = res.group(1)
+    else:   dsz = 'none'
+    
+    res  = re.search('\.sdram\s+(\d+)\s+(\d+)\n',  datain)
+    if res: ssz = res.group(1)
+    else:   ssz = 'none'
+          
+    #--------------------------------------------
+    #
+    #    Print results
+    #
+    sep    = '+--------------+--------------+--------------+--------------+'
+    header = '|      code    |       bss    |      data    |     sdram    |'
+    
+    fw    = 10                                         # field width
+    
+    code  = '|' + ' '*(fw - len(csz)) + csz + '    '
+    bss   = '|' + ' '*(fw - len(bsz)) + bsz + '    '
+    data  = '|' + ' '*(fw - len(dsz)) + dsz + '    '
+    sdram = '|' + ' '*(fw - len(ssz)) + ssz + '    |'
+    
+    print sep
+    print header
+    print sep
+    print code + bss + data + sdram
+    print sep
 #-------------------------------------------------------------------------------
 
